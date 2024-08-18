7dd97f7b90a6be9d2915ce38b1e3bbdce689b300
STR <ich@stefan-reis.com>
2024-08-18 15:03:00 +0200

    first auth draft

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,43 @@
-import Image from "next/image";
-import { Main } from "./components/Main";
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  const showSession = () => {
+    if (status === "authenticated") {
+      return (
+        <button
+          className="border border-solid border-black rounded"
+          onClick={() => {
+            signOut({ redirect: false }).then(() => {
+              router.push("/");
+            });
+          }}
+        >
+          Sign Out
+        </button>
+      );
+    } else if (status === "loading") {
+      return <span className="text-[#888] text-sm mt-7">Loading...</span>;
+    } else {
+      return (
+        <Link
+          href="/login"
+          className="border border-solid border-black rounded"
+        >
+          Sign In
+        </Link>
+      );
+    }
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left align-middle justify-center">
-        <Main />
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-xl">Home</h1>
+      {showSession()}
     </main>
   );
 }
